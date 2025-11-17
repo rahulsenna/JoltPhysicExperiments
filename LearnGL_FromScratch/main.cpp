7e4bfb8dbf876c63658ff896cf3e74511a036ecc
Rahul Senna <rahulvsenna@gmail.com>
2025-11-17 20:46:46 +0530

    glfw Example Triangle

--- LearnGL_FromScratch/main.cpp
+++ LearnGL_FromScratch/main.cpp
@@ -1,7 +1,139 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <OpenGL/gl3.h>
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
 #include <iostream>
+
+#include "linmath.h"
+
+typedef struct Vertex
+{
+  vec2 pos;
+  vec3 col;
+} Vertex;
+
+static const Vertex vertices[3] =
+    {{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
+     {{0.6f, -0.4f}, {0.f, 1.f, 0.f}},
+     {{0.f, 0.6f}, {0.f, 0.f, 1.f}}};
+
+const char *vertex_shader_text = R"(
+    #version 330
+    uniform mat4 MVP;
+    in vec3 vCol;
+    in vec2 vPos;
+    out vec3 color;
+    void main()
+    {
+        gl_Position = MVP * vec4(vPos, 0.0, 1.0);
+        color = vCol;
+    }
+)";
+const char *fragment_shader_text = R"(
+    #version 330
+    in vec3 color;
+    out vec4 fragment;
+    void main()
+    {
+        fragment = vec4(color, 1.0);
+    }
+)";
+
+static void error_callback(int error, const char *description)
+{
+  fprintf(stderr, "Error: %s\n", description);
+}
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main()
 {
-  std::cout << "Hello, Sailor" << '\n';
-  return (0);
+  glfwSetErrorCallback(error_callback);
+
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(640, 480, "OpenGL Triangle", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glfwSetKeyCallback(window, key_callback);
+
+  glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+
+  // NOTE: OpenGL error checks have been omitted for brevity
+
+  GLuint vertex_buffer;
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+
+  const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  const GLuint program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  const GLint mvp_location = glGetUniformLocation(program, "MVP");
+  const GLint vpos_location = glGetAttribLocation(program, "vPos");
+  const GLint vcol_location = glGetAttribLocation(program, "vCol");
+
+  GLuint vertex_array;
+  glGenVertexArrays(1, &vertex_array);
+  glBindVertexArray(vertex_array);
+  glEnableVertexAttribArray(vpos_location);
+  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, pos));
+  glEnableVertexAttribArray(vcol_location);
+  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, col));
+
+  while (!glfwWindowShouldClose(window))
+  {
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    const float ratio = width / (float)height;
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    mat4x4 m, p, mvp;
+    mat4x4_identity(m);
+    mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+    mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    mat4x4_mul(mvp, p, m);
+
+    glUseProgram(program);
+    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)&mvp);
+    glBindVertexArray(vertex_array);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
